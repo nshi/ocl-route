b1b97f1d1460ec356cb433366e5139ef32dcff47
Ning Shi <nshi@voltdb.com>
2013-06-29 14:21:15 -0400

    Initial Commit

---
+++ OpenCL Routing/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OpenCL Routing
+//
+//  Created by Ning Shi on 6/29/13.
+//  Copyright (c) 2013 Ning Shi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
